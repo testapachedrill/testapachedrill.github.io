aedfe911b64f68f06ae3ce2b6cbc672baaf5607e
Danny <dkirschner@batchblue.com>
2015-03-22 14:42:04 -0400

    refactoring sidebar js, including correct css stylesheets

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,24 @@
-// sidebar javascript
 $(function(){
-
-  $('li[class^=toctree]')
-    .filter(function(){ 
-      return $(this).next('ul').length > 0;
-    })
-      .on("click", function(){
-        $(".sphinxsidebar ul").find(".current").removeClass("current");
-        $(this).parents(".toctree-l1").addClass("current"); // or this addClass current
-        $.each( $(this).next("ul"), function(i){
-          $(this).toggle("slide");
-        })
-      })
+  watchDocTocClicks();
 })
+
+function watchDocTocClicks() {
+  $('li[class^=toctree]')
+  .filter(function(){ 
+    return $(this).next('ul').length > 0;
+  })
+  .on("click", function(){
+    make_current(this);
+    $.each( $(this).next("ul"), function(i){
+      $(this).toggle("slide");
+    })
+  })
+}
+
+function make_current(that) {
+  $(".current").removeClass("current");
+  var l1 = $(that).parents(".toctree-l1").addClass("current");
+  $(that).addClass("current");
+}
+
 
