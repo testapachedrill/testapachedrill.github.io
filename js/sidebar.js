69f2aafc5e88b27c388b91d2cbfe1e9812cf4543
Danny <dkirschner@batchblue.com>
2015-03-22 13:54:48 -0400

    adding current class to ul section clicked on docs toc menu. moving sidebar styles into docpage styles. keeping breadcrumbs and responsive sitewide styles in their respective stylesheets

--- js/sidebar.js
+++ js/sidebar.js
@@ -6,6 +6,8 @@ $(function(){
       return $(this).next('ul').length > 0;
     })
       .on("click", function(){
+        $(".sphinxsidebar ul").find(".current").removeClass("current");
+        $(this).parents(".toctree-l1").addClass("current"); // or this addClass current
         $.each( $(this).next("ul"), function(i){
           $(this).toggle("slide");
         })
